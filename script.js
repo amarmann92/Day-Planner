8e5286510d6ce27845b0ceb5aa226eeca1bcb15c
amar <amarmann92@gmail.com>
2021-04-02 17:03:58 -0700

    color coded hours

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 var day = document.getElementById("day")
 var currentDay= moment().format("MMMM Do YYYY")
 day.textContent= currentDay
+const hour= new Date().getHours()
+
+const textareas= $("textarea")
+
+for (let i = 0; i < textareas.length; i++) {
+    if (i+9<hour) {
+        $(textareas[i]).addClass("past")
+    } else if (i+9>hour) {
+        $(textareas[i]).addClass("future")
+    } else {
+        $(textareas[i]).addClass("present")
+    }
+}
 
 $(".saveBtn").on("click",function(){
     console.log($(this).prev().val())
@@ -10,3 +23,9 @@ $(".saveBtn").on("click",function(){
 })
 
 const times= ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"]
+for (let i = 0; i < times.length; i++) {
+    const gettime= localStorage.getItem(times[i])
+    if (gettime) {
+        $(`#${times[i]}`).val(gettime)
+    }
+}
